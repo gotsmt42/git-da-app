f31f71a49a15c2bd7ee2865e883e99da14e82084
da-app <baitonggot88@gmail.com>
2024-04-02 09:56:32 +0700

    add sample index page

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config(); // เรียกใช้ dotenv เพื่อโหลด Environment Variables
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRouter = require('./routes/auth');
+const productRouter = require('./routes/product');
+const fileRouter = require('./routes/file');
+
+const app = express();
+const PORT = process.env.APP_PORT;
+
+// ใช้ Environment Variables จากไฟล์ .env
+const apiURL = process.env.APP_API_URL;
+const apiKey = process.env.APP_API_KEY;
+const secret = process.env.APP_SECRET;
+
+
+console.log({apiURL});
+console.log({apiKey});
+console.log({secret});
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRouter);
+app.use('/api/product', productRouter);
+app.use('/api/files', fileRouter);
+app.use('/api/asset/uploads/images', express.static(__dirname + '/asset/uploads/images'));
+app.use('/api/asset/uploads/files', express.static(__dirname + '/asset/uploads/files'));
+
+app.use('/api/asset/image', express.static(__dirname + '/asset/image'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
