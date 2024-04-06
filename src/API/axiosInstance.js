bcbd7cdc2ba4a393dcf461a422411c4d3bd96a1d
da-app <baitonggot88@gmail.com>
2024-04-06 12:53:19 +0700

    dadsad

--- src/API/axiosInstance.js
+++ src/API/axiosInstance.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // ใช้ Environment Variable ที่ชื่อ APP_API_URL
   headers: {
     Accept: "application/json", // แก้ไข Content-Type ให้เป็น application/json
-    "X-API-Key": process.env.REACT_APP_API_KEY, // เพิ่ม API Key เข้าไปใน header หากต้องการ
+    // "X-API-Key": process.env.REACT_APP_API_KEY, // เพิ่ม API Key เข้าไปใน header หากต้องการ
     "X-Secret": process.env.REACT_APP_SECRET // เพิ่ม Secret Key เข้าไปใน header หากต้องการ
   },
 
